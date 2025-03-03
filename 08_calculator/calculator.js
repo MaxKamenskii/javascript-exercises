e390ca23659fb232aaddb4ca6884807090a6d1cc
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-03-03 21:42:25 +0500

    Make add, subtract, sum and multiply function in calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,32 @@
 const add = function(a, b) {
-	a + b
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	const sum = a.reduce(((initial, num) => initial + num), 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  const mult = a.reduce(((initial, num) => initial * num), 1);
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a) {
+	const pwr = a.reduce(function(initial, num){
+    let result = initial;
+    if(num > 0){
+      for(let i = 1; i < num; i++){
+        result *= initial;
+      }
+    }
+    return result;
+  })
+  return pwr;
 };
 
 const factorial = function() {
